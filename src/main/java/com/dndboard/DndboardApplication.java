1e3a5a367d30c33886bc14e69d502dcd3c6ccbcd
NeuronaBens <neuronabens@gmail.com>
2022-02-26 12:53:22 -0500

    Update DndboardApplication.java

--- src/main/java/com/dndboard/DndboardApplication.java
+++ src/main/java/com/dndboard/DndboardApplication.java
@@ -2,10 +2,24 @@ package com.dndboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@RestController
 public class DndboardApplication {
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DndboardApplication.class, args);
 	}
